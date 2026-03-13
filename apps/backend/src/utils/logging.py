4248b0878d08715a57dc7c42b96f1254ef3205e5
Aryan-lomte05 <aryan.lomte@somaiya.edu>
2026-03-13 21:18:27 +0530

    chore: remove legacy apps directory and empty folders

--- apps/backend/src/utils/logging.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-import sys
-
-
-# Hinglish: Pretty logging — prod mein rotate/serialize add kar sakte ho.
-logger.remove()
-logger.add(sys.stderr, level="INFO", colorize=True)
